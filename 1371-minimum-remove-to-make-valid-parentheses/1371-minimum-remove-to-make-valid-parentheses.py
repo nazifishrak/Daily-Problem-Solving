fd3ff7e7b7ba569428f8829f7e416e5cf21f3193
Syed Nazif Ishrak <63534571+nazifishrak@users.noreply.github.com>
2024-10-17 21:51:52 -0700

    Time: 1328 ms (27.78%), Space: 16.2 MB (42.22%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -4,32 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        balance = 0
-        index = []
 
-        for p1 in range(len(s)):
-            if s[p1] == "(":
-                balance += 1
-            elif s[p1] == ")":
-                if balance == 0:
-                    index.append(p1)  
-                else:
-                    balance -= 1
-        
-
-        balance = 0
-        for p1 in range(len(s)-1, -1, -1):
-            if s[p1] == "(":
-                if balance == 0:
-                    index.append(p1)  
-                else:
-                    balance -= 1
-            elif s[p1] == ")":
-                balance += 1
-
-        output = ""
+        stack=[]
+        invalid=set()
         for i in range(len(s)):
-            if i not in index:
-                output += s[i]
+            if s[i]=="(":
+                stack.append(i)
+            elif s[i]==")":
+                if len(stack) !=0:
+                    stack.pop()
+                else:
+                    invalid.add(i)
+        
+        for i in stack:
+            invalid.add(i)
+
+        output =""
+
+        for i,char in enumerate(s):
+            if i not in invalid:
+                output+=char
 
         return output
+            
+
+    
