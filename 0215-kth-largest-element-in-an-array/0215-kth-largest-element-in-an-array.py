a44e322d8c2e1332842dd659ed3bdc816ccdd2ec
Syed Nazif Ishrak <63534571+nazifishrak@users.noreply.github.com>
2024-11-10 12:55:19 -0800

    Time: 242 ms (67.23%), Space: 18.8 MB (85.7%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,31 +1,18 @@
+from heapq import heapify
 class Solution(object):
     def findKthLargest(self, nums, k):
         """
-        Find the kth largest element in an unsorted array using quickselect.
-        
-        Args:
-            nums: List[int] - input array
-            k: int - k value for kth largest element
-            
-        Returns:
-            int - kth largest element
+        Find the kth largest element in an unsorted array using heap
         """
-        def quick_select(nums, k):
-            if not nums:
-                return None
-            if len(nums) == 1:
-                return nums[0]
-            
-            pivot = nums[len(nums) // 2]  # Choose middle element as pivot to avoid worst case
-            left_part = [n for n in nums if n > pivot]
-            right_part = [n for n in nums if n < pivot]
-            equal_part = [n for n in nums if n == pivot]
-            
-            if k <= len(left_part):
-                return quick_select(left_part, k)
-            elif k <= len(left_part) + len(equal_part):
-                return pivot
+
+        heap=nums[0:k]
+        heapq.heapify(heap)
+        for i in nums[k:]:
+            if i>heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap,i)
             else:
-                return quick_select(right_part, k - len(left_part) - len(equal_part))
-        
-        return quick_select(nums, k)
+                continue
+
+        return heap[0]
+
